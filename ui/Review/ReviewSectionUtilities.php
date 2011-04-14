ebbbfcd168eb61021aa062c21872a7ad9686aebc
yehster <kevinyeh@alum.mit.edu>
2011-04-14 18:41:19 -0400

    modified:   ui/Editor/Review.css 	modified:   ui/Review/ReviewSectionUtilities.php

--- ui/Review/ReviewSectionUtilities.php
+++ ui/Review/ReviewSectionUtilities.php
@@ -1,22 +1,24 @@
 <?php
 
-function reviewRecurse($em,$DOM,$parent,$DocItem)
+function reviewRecurse($em,$DOM,$parent,$DocItem,$depth)
 {
     $DocEntry=$DocItem->getEntry();
     $text=htmlentities($DocEntry->getText());
     $nextParent=$DOM->createElement("DIV",$text);
-    $nextParent->setAttribute("entrytype",$DocEntry->getType());
+    $nextParent->setAttribute("reviewtype",$DocEntry->getType());
+    $nextParent->setAttribute("depth",$depth);
+
     $parent->appendChild($nextParent);
     foreach($DocItem->getItems() as $childItem)
     {
-            reviewRecurse($em,$DOM,$nextParent,$childItem);
+            reviewRecurse($em,$DOM,$nextParent,$childItem,$depth+1);
     }
 }
 function generateReviewDOM($em,$DOM,$section)
 {
     $retVal = $DOM->createElement("SPAN");
     $DOM->appendChild($retVal);
-    reviewRecurse($em,$DOM,$retVal,$section->getItem());
+    reviewRecurse($em,$DOM,$retVal,$section->getItem(),1);
     return $retVal;
 }
 
