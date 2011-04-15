29c9c4f40d74fc222ae6f09f20a820dade51eebf
Kevin Yeh <kevinyeh@alum.mit.edu>
2011-04-14 21:24:36 -0400

    modified:   ui/Editor/Review.css 	modified:   ui/Review/ReviewSectionUtilities.php

--- ui/Review/ReviewSectionUtilities.php
+++ ui/Review/ReviewSectionUtilities.php
@@ -1,15 +1,31 @@
 <?php
 
+function createEntry($DOM,$parent,$DocEntry,$depth,$type,$text)
+{
+    $newEntry = $DOM->createElement($type,$text);
+    $newEntry->setAttribute("reviewtype",$DocEntry->getType());
+    $newEntry->setAttribute("uuid",$DocEntry->getUUID());
+    $newEntry->setAttribute("depth",$depth);
+    $parent->appendChild($newEntry);
+
+    return $newEntry;
+}
+
+function createCheckbox($DOM,$parent,$DocEntry,$depth)
+{
+    $retVal=createEntry($DOM,$parent,$DocEntry,$depth,"INPUT","");
+    $retVal->setAttribute("TYPE","CHECKBOX");
+    $retVal->setAttribute("checked","true");
+    return $retVal;
+}
 function reviewRecurse($em,$DOM,$parent,$DocItem,$depth)
 {
     $DocEntry=$DocItem->getEntry();
     $text=htmlentities($DocEntry->getText());
-    $nextParent=$DOM->createElement("DIV",$text);
-    $nextParent->setAttribute("reviewtype",$DocEntry->getType());
-    $nextParent->setAttribute("depth",$depth);
-    $nextParent->setAttribute("uuid",$DocEntry->getUUID());
+    $nextParent=createEntry($DOM,$parent,$DocEntry,$depth,"div","");
+    $checkBox=createCheckbox($DOM,$nextParent,$DocEntry,$depth);
+    $info=createEntry($DOM,$nextParent,$DocEntry,$depth,"span",$text);
 
-    $parent->appendChild($nextParent);
     foreach($DocItem->getItems() as $childItem)
     {
             reviewRecurse($em,$DOM,$nextParent,$childItem,$depth+1);
