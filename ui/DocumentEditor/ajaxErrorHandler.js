126eadcf7283180f1c1e6774f65683dbd1f228fe
Kevin Yeh <kevinyeh@alum.mit.edu>
2011-06-17 00:31:18 -0400

    modified:   ui/DocumentEditor/ajaxErrorHandler.js

--- ui/DocumentEditor/ajaxErrorHandler.js
+++ ui/DocumentEditor/ajaxErrorHandler.js
@@ -2,6 +2,7 @@ function handleAjaxError(event,XHR,settings,thrownError)
 {
     msg = "Error:" + settings.url + ":" + XHR.responseText + ":" + thrownError;
     $("#status").html(msg);
+    window.status=msg;
 }
 
 
