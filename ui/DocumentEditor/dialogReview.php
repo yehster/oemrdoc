6c210d0df3efd4af46d3bb1a3a9adf293c9f1692
Kevin Yeh <kevinyeh@alum.mit.edu>
2011-07-20 04:38:56 -0400

    deleted:    ui/DocumentEditor/dialogReview.js 	deleted:    ui/DocumentEditor/dialogReview.php

--- ui/DocumentEditor/dialogReview.php
+++
@@ -1,6 +0,0 @@
-<section id="dialogReview" class="dialog" hidden="true">
-    <button id="closeReview">close</button>
-    <section id="reviewHistory"></section>
-    <button id="copyEntries">copy</button>
-    <section id="reviewCurrent"></section>
-</section>
