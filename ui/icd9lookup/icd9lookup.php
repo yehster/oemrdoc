77a7a72c2dd0938f5108a338cdb235ee7d6ce99e
Kevin Yeh <kevinyeh@alum.mit.edu>
2011-07-28 10:26:24 -0400

    modified:   ui/icd9lookup/icd9lookup.php

--- ui/icd9lookup/icd9lookup.php
+++ ui/icd9lookup/icd9lookup.php
@@ -4,6 +4,18 @@ include_once('/var/www/openemr/library/doctrine/init-em.php');
 <style>
 
 </style>
+
+<script src="../../../js/jquery-1.6.1.min.js"></script>
+<script src="icd9lookup.js"></script>
+<script>
+window.onload=registerICD9lookupEvents;    
+</script>
+<html lang="en">
+    <head>
+        <meta charset="utf-8"/>
+        <title>ICD9 Search - Integral EMR</title>
+    </head>
+<body>
 <section ID="problemDialog">
     <input type="text" id="txtProblem"/>
     <span>Filter</span>
@@ -12,9 +24,5 @@ include_once('/var/www/openemr/library/doctrine/init-em.php');
     <section ID="problemFavorites"/> </section>
     <section ID="problemSearch"/> </section>       
 </section>
-
-<script src="../../../js/jquery-1.6.1.min.js"></script>
-<script src="icd9lookup.js"></script>
-<script>
-window.onload=registerICD9lookupEvents;    
-</script>
+</body>
+</html>
