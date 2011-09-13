391897b49744e8ebcb9a85202f030eb3f26e3c0a
yehster <kevinyeh@alum.mit.edu>
2011-09-13 19:42:27 -0400

    sig dialog

--- ui/DocumentEditor/MedicationForms/dynamicMedSIGDialog.php
+++ ui/DocumentEditor/MedicationForms/dynamicMedSIGDialog.php
@@ -1,17 +1,23 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of dynamicMedSIGDialog
- *
- * @author yehster
- */
-class dynamicMedSIGDialog {
-    //put your code here
+function generateMedSIGDialog($DOM,$medEntry)
+{
+    // Display Drug Info at top, then the input fields
+    $retval=$DOM->createElement("section");
+    $drugInfo=$DOM->createElement("section");
+    $drugInfo->setAttribute("class","SIGdrugInfo");
+    $retval->appendChild($drugInfo);
+    
+    $drugLabel->$DOM->createElement("span",$medEntry->getText());
+    
+    //alternate drug info?
+    
+    $cancelButton=$DOM->createElement("button","cancel");
+    $cancelButton->setAttribute("class","cancelSIG");
+    $drugInfo->appendChild($cancelButton);
+    
+    
+    return $retval;
 }
 
 ?>
