8b290cccf45844d1ffcbacd08ee432babda8305a
Kevin Yeh <kevinyeh@alum.mit.edu>
2011-09-28 08:17:35 -0400

    allscripts

--- greasemonkey/allscriptsInterface.user.js
+++ greasemonkey/allscriptsInterface.user.js
@@ -9,9 +9,36 @@
 // ==/UserScript==
 
 
-var interstitial="https://eprescribe.allscripts.com/InterstitialAd.aspx";
+var pages={
+    interstitial: "/InterstitialAd.aspx",
+    addPatient: "/AddPatient.aspx?Mode=Add",
+    def: "/default.aspx",
+    allergy: "/PatientAllergy.aspx"
+}
+
+function resetInfo()
+{
+    GM_setValue("OpenEMR Server","");
+    GM_setValue("OpenEMR Session","");
+    
+    // Patient Info
+    GM_setValue("patientFNAME","");
+    GM_setValue("patientLNAME","");
+    GM_setValue("patientDOB","");
+    
+    // Prescription Info
+    GM_setValue("MedName","") // The Med Name
+    GM_setValue("MedSTR",""); // The Med Strength
+    GM_setValue("MedSIG",""); // The Med SIG
+    
+    GM_setValue("patientFound",false);
+    
+}
+
+//TODO: Can I add a dialog div that displays the drugs from OpenEMR?
+
 var loc=window.location.href;
-if(loc.indexOf(interstitial)==0)
+if(loc.indexOf(pages[interstitial])>=0)
     {
         var adButton = document.getElementById("adControl_closeButton");
         if (adButton != null)
