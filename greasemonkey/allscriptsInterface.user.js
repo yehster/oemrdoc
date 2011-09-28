c6a7c76dc5b0733a2b51b6eb2ec6e95fb4ca1f8d
yehster <kevinyeh@alum.mit.edu>
2011-09-28 17:45:55 -0400

    allscripts greasemonkey

--- greasemonkey/allscriptsInterface.user.js
+++ greasemonkey/allscriptsInterface.user.js
@@ -3,6 +3,8 @@
 // @namespace     http://yehster.no-ip.org/
 // @description   
 // @include       https://eprescribe.allscripts.com/*
+// @include       */openemr/interface/main/main_title.php
+// @include       */openemr/interface/patient_file/summary/demographics.php*
 // @exclude       
 // @exclude       
 // @require http://code.jquery.com/jquery-1.6.4.min.js
@@ -13,7 +15,9 @@ var pages={
     interstitial: "/InterstitialAd.aspx",
     addPatient: "/AddPatient.aspx?Mode=Add",
     def: "/default.aspx",
-    allergy: "/PatientAllergy.aspx"
+    allergy: "/PatientAllergy.aspx",
+    oemrMain: "/openemr/interface/main/main_title.php",
+    oemrDemo: "openemr/interface/patient_file/summary/demographics.php"
 }
 
 function resetInfo()
@@ -38,11 +42,24 @@ function resetInfo()
 //TODO: Can I add a dialog div that displays the drugs from OpenEMR?
 
 var loc=window.location.href;
-if(loc.indexOf(pages[interstitial])>=0)
+if(loc.indexOf(pages['interstitial'])>=0)
     {
         var adButton = document.getElementById("adControl_closeButton");
         if (adButton != null)
         {
             adButton.click();
         }
+    
+    }
+
+
+if(loc.indexOf(pages['oemrMain'])>=0)
+    {
+    }
+
+if(loc.indexOf(pages['oemrDemo'])>=0)
+    {
+        titleFrame=unsafeWindow.top;
+//        curPat=unsafeWindow.$("#current_patient",unsafeWindow.parent.title);
+        window.alert(unsafeWindow.$(titleFrame).html());
     }
