f4c53b56877c0014334843c16b98e64357c8c448
Kevin Yeh <kevinyeh@alum.mit.edu>
2011-11-24 02:42:26 -0500

    billing

---
+++ sql/data/insert_cpt.sql
@@ -0,0 +1,20 @@
+insert into codes(id,code_text,code,code_type,superbill,active)
+values ('14461', 'Minimal - Established', '99211', '1', '10', '1');
+insert into codes(id,code_text,code,code_type,superbill,active)
+values ('14462', 'Problem Focued - Established', '99212', '1', '10', '1');
+insert into codes(id,code_text,code,code_type,superbill,active)
+values ('14463', 'Expanded Problem focused - Established', '99213', '1', '10', '1');
+insert into codes(id,code_text,code,code_type,superbill,active)
+values ('14464', 'Detailed - Established', '99214', '1', '10', '1');
+insert into codes(id,code_text,code,code_type,superbill,active)
+values ('14465', 'Comprehensive - Established', '99215', '1', '10', '1');
+insert into codes(id,code_text,code,code_type,superbill,active)
+values ('14466', 'Minimal - New', '99201', '1', '10', '1');
+insert into codes(id,code_text,code,code_type,superbill,active)
+values ('14467', 'Problem Focued - New', '99202', '1', '10', '1');
+insert into codes(id,code_text,code,code_type,superbill,active)
+values ('14468', 'Expanded Problem focused - New', '99203', '1', '10', '1');
+insert into codes(id,code_text,code,code_type,superbill,active)
+values ('14469', 'Detailed - New', '99204', '1', '10', '1');
+insert into codes(id,code_text,code,code_type,superbill,active)
+values ('14470', 'Comprehensive - New', '99205', '1', '10', '1');
