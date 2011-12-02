59352dcb812a45b3c6f5cb637a199a9d9b729895
yehster <kevinyeh@alum.mit.edu>
2011-12-02 16:45:17 -0500

    med sig

--- interface/med/defaultSIGValues.php
+++ interface/med/defaultSIGValues.php
@@ -1,7 +1,70 @@
 <?php
 
+function findAttribute($arrAtt,$atn)
+{
+    for($idx=0;$idx<count($arrAtt);$idx++)
+    {
+        if($arrAtt[$idx]->getATN()==$atn)
+        {
+            return $arrAtt[$idx]->getATV();
+        }
+    }
+    return null;
+}
+
+function determineUnits($dst,$ddf,$drta)
+{
+    
+    $loc=strpos($dst,'/');
+    if($loc)
+    {
+
+        if(strpos($dst,"units"))
+        {
+            return "units";
+        }
+        elseif(strpos($dst,"mg"))
+        {
+            if(strpos($dst,"mL"))
+            {
+                return "mL";                    
+            }
+        }
+        elseif($drta==="INH")
+        {
+                return "puffs";                                
+        }
+    }
+    else
+    {
+        if($drta==="OPH")
+        {
+            return "gtt";
+        }
+        elseif($drta==="INH")
+        {
+            
+        }
+        else
+        {
+            return $ddf;
+        }
+    }    
+}
+
 function setDefaultSIGValues($em,$sig,$med)
 {
+    $attributes=$em->getRepository('library\doctrine\Entities\RXNORM\DrugAttribute')->findByrxcui($med->getRXCUI());
+    
+    $dst=findAttribute($attributes,"DST");
+    $ddf=findAttribute($attributes,"DDF");
+    $drta=findAttribute($attributes,"DRTA");
+    
+    $sig->setUnits(determineUnits($dst,$ddf,$drta));
+    $sig->setRoute($drta);
+    
+    $sig->setSchedule("Daily");
+    $sig->setQuantity("1");
     
 }
 ?>
