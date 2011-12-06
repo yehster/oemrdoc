89edfb695cb1bbf70603252c3ecce71bb100a5a4
yehster <kevinyeh@alum.mit.edu>
2011-12-06 11:09:42 -0500

    allergy widget

--- queries/AllergiesQueries.php
+++ queries/AllergiesQueries.php
@@ -3,14 +3,16 @@
 function findSubEntries($em,$pat,$code,$code_type)
 {
         $qb=$em->createQueryBuilder()
-        ->select("i.items")
+        ->select("d")
         ->from("library\doctrine\Entities\DocumentEntry","d")
         ->join("d.item","i")
-        ->where("d.patient=:pat")
-        ->andWhere("d.code=:code")
-        ->andWhere("d.code_type:=code_type")
-        ->groupBy("a.text")
-        ->orderBy("a.modified","desc");
+        ->join("i.parent","pi")
+        ->join("pi.entry","t")
+        ->where("t.patient=:pat")
+        ->andWhere("t.code=:code")
+        ->andWhere("t.code_type=:code_type");
+//        ->groupBy("a.text")
+//        ->orderBy("a.modified","desc");
         
         // Specify parameters for the parent entry
         $qb->setParameter("pat",$pat);
