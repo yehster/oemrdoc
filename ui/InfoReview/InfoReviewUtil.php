adc2388f447455b201098f8a5ecdf5b81bc6c2d0
yehster <kevinyeh@alum.mit.edu>
2011-12-09 17:14:28 -0500

    info review

--- ui/InfoReview/InfoReviewUtil.php
+++ ui/InfoReview/InfoReviewUtil.php
@@ -1,8 +1,11 @@
 <?php
+require_once("$doctrineroot/queries/AllergiesQueries.php");
 function ReviewInfo($em,$DOM,$pat,$section)
 {
 
     $retval=$DOM->createElement("span","hello world!");
+    $info=findSubEntries($em,$pat,$section->getCode(),$section->getCode_type());
+    echo count($info);
     $DOM->appendChild($retval);
 }
 ?>
