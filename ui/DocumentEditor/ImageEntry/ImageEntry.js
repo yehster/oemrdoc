df84ee8a9d8fbddf366d27f9e76cceae95ab8112
Kevin Yeh <kevinyeh@alum.mit.edu>
2012-01-25 23:09:00 -0500

    canvas method

--- ui/DocumentEditor/ImageEntry/ImageEntry.js
+++ ui/DocumentEditor/ImageEntry/ImageEntry.js
@@ -1,24 +1,16 @@
 // Drawing Functions go below here
 var canvas_states=new Array();
-function updateCanvas(canvasUUID)
-{
-    canvas_state=canvas_states[$(this).attr("canvasUUID")];
-    canvas_state.num++;
-    canvas=$("canvas[canvasuuid='"+canvasUUID+"']");
-    ShowStatusMessage(canvas_state.num+":"+canvas.length);
-    canvas.get(0).getContext("2d").stroke();
-}
 function startDraw(e)
 {
     canvas_state = new Object();
     canvas_state.drawing=true;
     canvas_state.num=0;
+    canvas_state.point_count=0;
     mouseX = e.pageX-$(this).offset().left;
     mouseY = e.pageY-$(this).offset().top;
     canvasUUID=$(this).attr("canvasUUID");
     canvas_state.X=mouseX;
     canvas_state.Y=mouseY;
-    canvas_state.strokeTimer=setInterval("updateCanvas('"+canvasUUID+"')",200);
     context = this.getContext("2d");
     canvas_states[$(this).attr("canvasUUID")]=canvas_state;
     context.beginPath();
@@ -34,6 +26,16 @@ function contDraw(e)
             mouseX = e.pageX-$(this).offset().left;
             mouseY = e.pageY-$(this).offset().top;
             context.lineTo(mouseX,mouseY);
+            canvas_state.point_count++;
+            distance=Math.pow(mouseX-canvas_state.X,2)+Math.pow(mouseY-canvas_state.Y,2);
+            ShowStatusMessage(distance);
+            if((distance>=9) || (canvas_state.point_count>5))
+                {
+                    context.stroke();
+                    canvas_state.point_count=0;
+                    canvas_state.X=mouseX;
+                    canvas_state.Y=mouseY;
+                }
         }
 
 }
@@ -44,7 +46,6 @@ function endDraw(evt)
     canvas_states[$(this).attr("canvasUUID")]=canvas_state;
             context = this.getContext("2d");
             context.stroke();
-            clearInterval(canvas_state.strokeTimer);
 }
 function canvasEvents(parent)
 {
