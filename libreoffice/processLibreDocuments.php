e55da9a33ba48a2a97012a97e825384d9089251c
Kevin Yeh <kevinyeh@alum.mit.edu>
2012-02-21 18:49:41 -0500

    error handling libre

--- libreoffice/processLibreDocuments.php
+++ libreoffice/processLibreDocuments.php
@@ -2,10 +2,19 @@
 
 function generateXMLFromDocument($filename,$path)
 {
-    echo $path;
     $shell_cmd="sh " .$GLOBALS['doctrineroot']."/shell/io.sh". " ".$filename. " ".$path;
     $shell_cmd=escapeshellcmd($shell_cmd);
-    exec($shell_cmd,$output,$ret_val);
-    echo "\n".$ret_val."\n";
+    $last_output=exec($shell_cmd,$output,$ret_val);
+    if($ret_val!="0")
+    {
+        foreach($output as $line)
+        {
+            echo $line."\n";
+        }
+    }
+    else
+    {
+        $filename=$last_output;
+    }
 }
 ?>
