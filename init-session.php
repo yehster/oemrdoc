2999adf7d2f34dc84264800110f6f43c1a87c332
Kevin Yeh <kevinyeh@alum.mit.edu>
2012-06-27 19:44:19 -0400

    more robust doctrine session initialization

--- init-session.php
+++ init-session.php
@@ -3,13 +3,15 @@
 session_name("OpenEMR");
 session_start();
 
-if ((time() - $_SESSION["last_update"]) > 7200) {
-    $site_id=$_SESSION['site_id'];
+if (empty($_SESSION["last_update"]) || ((time() - $_SESSION["last_update"]) > 7200)) {
+    $site_id=isset($_SESSION['site_id']) ? $_SESSION['site_id'] : "default";
     session_unset();
     session_destroy();
     unset($_COOKIE[session_name()]);
-    echo "<script>top.location='/openemr/interface/login/login_frame.php?error=1&site=".$site_id."'</script>";
-    return;
+    $new_location="/openemr/interface/login/login_frame.php?error=1&site=".$site_id;
+//    echo "<script>top.location='".$new_location."'</script>";
+    header("Location:".$new_location);
+    exit;
 }
 else
 {
