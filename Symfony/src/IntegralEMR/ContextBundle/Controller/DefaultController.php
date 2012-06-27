e1d41f03ba829169f12f799dfca3d6472b271a6e
Kevin Yeh <kevinyeh@alum.mit.edu>
2012-06-27 17:32:15 -0400

    session tweaks

--- Symfony/src/IntegralEMR/ContextBundle/Controller/DefaultController.php
+++ Symfony/src/IntegralEMR/ContextBundle/Controller/DefaultController.php
@@ -11,19 +11,8 @@ class DefaultController extends Controller
 {
     public function indexAction($uuid,Request $request)
     {
-        $session = $request->getSession();
-        $last_update=$session->get("last_update");
-        $elapsed=time() - intval($last_update);
-        if  ($elapsed > 10)
-        {
-            $site_id=$session->get('site_id');
-            $login="/openemr/interface/login/login_frame.php?error=1&site=".$site_id.":".$last_update.":".$elapsed.":".$session->getId();
-            $session->set("last_update",time());
-            return new Response($login) ;
-            
-        }
-        require_once("/var/www/openemr/library/doctrine/init-em.php");
-        $doc = $em->getRepository('library\doctrine\Entities\Document')->find($uuid);
+
+        $doc = $GLOBALS['em']->getRepository('library\doctrine\Entities\Document')->find($uuid);
         
         $test=$request->get("test","default");
         if($doc==null)
